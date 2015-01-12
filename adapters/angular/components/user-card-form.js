5646ce579c719a613ebb0216780c3c7a9cd81c94
加缪 <jiamiu.hzy@alibaba-inc.com>
2015-01-12 16:41:08 +0800

    change demo user-card-form template url from gh-pages

--- adapters/angular/components/user-card-form.js
+++ adapters/angular/components/user-card-form.js
@@ -1,8 +1,7 @@
 angular.module("demo",["Gilgamesh"])
 .component("userCardForm", function(){
     return {
-      //for http://sskyy.github.io/Gilgamesh
-      templateUrl : "/Gilgamesh/adapters/angular/components/user-card-form.html",
+      templateUrl : "/adapters/angular/components/user-card-form.html",
       link : function( $scope, $el){
 
       }
